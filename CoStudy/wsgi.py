7454f5e53c9891f3dc91d072c228092ca39f5549
Kasamens <@gmail.com>
2018-07-18 08:53:44 -0400

    Update

--- CoStudy/wsgi.py
+++ CoStudy/wsgi.py
@@ -3,7 +3,7 @@ import os
 from django.core.wsgi import get_wsgi_application
 from whitenoise.django import DjangoWhiteNoise
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "costudy.settings.production")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "co-study.settings.production")
 
 application = get_wsgi_application()
 application = DjangoWhiteNoise(application)
