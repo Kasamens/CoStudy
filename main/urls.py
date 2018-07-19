0a0bfdc7df8c9e175aeb54e27496d6a21b475080
Kasamens <@gmail.com>
2018-07-19 08:27:06 -0400

    Modified runtime.txt

--- main/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-from .models import Post
-
-app_name = 'main'
-urlpatterns = [
-    path('', views.IndexView.as_view(thought = Post), name='index'),
-    path('<int:pk>/', views.ThoughtDetail.as_view(), name='detail')
-]
