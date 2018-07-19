6ac3362f7706ad056fbb8160b1c79f6bfdc328dd
Kasamens <@gmail.com>
2018-07-19 11:36:22 -0400

    Added send_js function

--- app.py
+++ app.py
@@ -46,11 +46,11 @@ def send_js(path):
     return send_from_directory('js', path)
 
 @app.route('/img/<path:path>')
-def img_js(path):
+def send_img(path):
     return send_from_directory('img', path)
 
 @app.route('/css/<path:path>')
-def css_js(path):
+def send_css(path):
     return send_from_directory('css', path)
 
 @app.route('/posts')
